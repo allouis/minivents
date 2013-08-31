08c846f332ac35d009759f2dfd94965fe17bce6a
Fabien <fabienallouis@gmail.com>
2013-08-31 11:52:57 +0100

    Added ability to mixin events to other objects

--- minivents.js
+++ minivents.js
@@ -1,34 +1,33 @@
 /**
  *  Events
  */
-function Events(){
+function Events(target){
   var events = {}, i, list, args, A = Array;
-  return {
+  target || target = this;  
     /**
      *  On: listen to events
      */
-    on: function(type, func, ctx){
+    target.on = function(type, func, ctx){
       events[type] || (events[type] = [])
       events[type].push({f:func, c:ctx})
-    },
+    }
     /**
      *  Off: stop listening to event / specific callback
      */
-    off: function(type, func){
+    target.off = function(type, func){
       list = events[type] || []
       !func && (list.length = 0) 
       i = list.length
       while(~--i<0) func == list[i].f && list.splice(i,1)
-    },
+    }
     /** 
      * Emit: send event, callbacks will be triggered
      */
-    emit: function(){
+    target.emit = function(){
       args = A.apply([], arguments)
       list = events[args.shift()] || []
       args = args[0] instanceof A && args[0] || args
       i = list.length
       while(~--i<0) list[i].f.apply(list[i].c, args)
     }
-  }
 }
