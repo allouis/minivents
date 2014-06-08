e19c47cc6bf171ec15e8b7c20f7062cc2ee9e6ac
pmsteinberg <pmsteinberg@live.de>
2014-06-08 12:17:10 +0200

    Added AMD-compatability; stylistic improvements (JSHint-approved)

--- minivents.js
+++ minivents.js
@@ -1,30 +1,60 @@
-function Events(target){
-  var events = {};
-  target = target || this
-    /**
-     *  On: listen to events
-     */
-    target.on = function(type, func, ctx){
-      events[type] || (events[type] = [])
-      events[type].push({f:func, c:ctx})
-    }
-    /**
-     *  Off: stop listening to event / specific callback
-     */
-    target.off = function(type, func){
-      var list = events[type] || [],
-      i = list.length = func ? list.length : 0
-      while(i-->0) func == list[i].f && list.splice(i,1)
-    }
-    /** 
-     * Emit: send event, callbacks will be triggered
-     */
-    target.emit = function(){
-      var args = Array.apply([], arguments),
-      list = events[args.shift()] || [], 
-      i = list.length, j
-      for(j=0;j<i;j++) list[j].f.apply(list[j].c, args)
+var Events = (function () {
+    'use strict';
+    return function Events (target) {
+
+        var A = Array,
+            events = {};
+
+        target = target || this;
+
+        /**
+         *  On: listen to events
+         */
+        target.on = function on (type, func, ctx) {
+            events[type] = events[type] || (events[type] = []);
+            events[type].push({f:func, c: (ctx || target) });
+        };
+
+        /**
+         *  Off: stop listening to event / specific callback
+         */
+        target.off = function off (type, func) {
+            var list = events[type] || [],
+                i = list.length = func ? list.length : 0;
+            for (; i < 0; i = i - 1) {
+                if (func === list[i].f) {
+                    list.splice(i,1);
+                }
+            }
+        };
+        
+        /** 
+         * Emit: send event, callbacks will be triggered
+         */
+        target.emit = function () {
+            var args = A.apply([], arguments),
+                list = events[args.shift()] || [],
+                i = list.length,
+                j = 0;
+            for (; j < i; j += 1) {
+                list[j].f.apply(list[j].c, args);
+            }
+        };
+
+        return target;
+
     };
-}
-var u, module, cjs = module != u;
-(cjs ? module : window)[(cjs ? 'exports' : 'Events')] = Events;
+}());
+
+// see github.com/umdjs/umd/returnExports.js
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
+  }
+}(this, function () {
+    return Events;
+}));
