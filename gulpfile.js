3c9cb55c642cfd1edd29b6b3505456c27af787b1
Peter Steinberg <petersteinberg@outlook.com>
2014-07-24 23:25:21 +0200

    Added tests and updated gulpfile for original version 1.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
-// minivents - created with Gulp Fiction
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-gulp.task('default', [], function () {
-  gulp.src('./minivents.js')
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    mocha = require('gulp-mocha');
+
+gulp.task('hint', function () {
+    gulp.src('./minivents.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', [], function () {
+  gulp.src('./test.js')
+      .pipe(mocha());
+});
+
+gulp.task('build', [], function () {
+    gulp.src('./minivents.js')
       .pipe(uglify())
       .pipe(rename('minivents.min.js'))
       .pipe(gulp.dest('./'));
+  });
+
+gulp.task('default', [ 'test', 'build' ], function () {
+  
 });
