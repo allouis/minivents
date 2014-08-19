9da16a26297af6674f32f80e9e2a988a9db35c30
Remy Sharp <remy@remysharp.com>
2014-08-19 11:32:30 +0100

    BECAUSE COMPRESS ALL THE THINGS
    For fun...

--- minivents.js
+++ minivents.js
@@ -1,28 +1,28 @@
-(this.module||window)[(this.module ? 'exports' : 'Events')] = function (target){
-    var events = {};
-    target = target || this
-      /**
-       *  On: listen to events
-       */
-      target.on = function(type, func, ctx){
-        events[type] = events[type] || []
-        events[type].push({f:func, c:ctx})
-      }
-      /**
-       *  Off: stop listening to event / specific callback
-       */
-      target.off = function(type, func, list){
-        type || (events = {})
-        list = events[type] || [],
-        i = list.length = func ? list.length : 0
-        while(i-->0) func == list[i].f && list.splice(i,1)
-      }
-      /**
-       * Emit: send event, callbacks will be triggered
-       */
-      target.emit = function(args){
-        args = [].slice.call(arguments),
-        list = events[args.shift()] || [], i=0, j
-        for(;j=list[i];i++) j.f.apply(j.c, args)
-      };
-  };
+(this.window||module)[(this.window ? 'Events' : 'exports')] = function (target){
+  var events = {};
+  target = target || this
+    /**
+     *  On: listen to events
+     */
+    target.on = function(type, func, ctx){
+      events[type] = events[type] || []
+      events[type].push({f:func, c:ctx})
+    }
+    /**
+     *  Off: stop listening to event / specific callback
+     */
+    target.off = function(type, func, list){
+      type || (events = {})
+      list = events[type] || [],
+      i = list.length = func ? list.length : 0
+      while(i-->0) func == list[i].f && list.splice(i,1)
+    }
+    /**
+     * Emit: send event, callbacks will be triggered
+     */
+    target.emit = function(){
+      var args = [].slice.call(arguments),
+      list = events[args.shift()] || [], i=0, j
+      for(;j=list[i];i++) j.f.apply(j.c, args)
+    };
+}
