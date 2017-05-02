dca862bd28a8503eb7ee7423aed6e08b371988e9
Robert Akhmerov <zored.box@gmail.com>
2017-05-02 11:59:17 +0300

    Add typescript support. (#34)
    * Add support of types.
    
    * Add package.json types.

---
+++ types/minievents.d.ts
@@ -0,0 +1,35 @@
+/**
+ * Add support of events to class.
+ */
+export class Events {
+    /**
+     * Listen to events.
+     *
+     * @param type
+     * @param callback
+     * @param context
+     */
+    on(type: string, callback: Function, context: any[])
+
+    /**
+     * Stop listening to events or specific callback.
+     *
+     * @param type
+     * @param callback
+     */
+    off(type: string = null, callback: Function = null)
+
+    /**
+     * Send event. Callbacks will be triggered.
+     *
+     * @param type
+     */
+    emit(type: string)
+}
+
+/**
+ * Add support of events to object.
+ *
+ * @param object
+ */
+export function Events<T>(object: T): T & Events;
